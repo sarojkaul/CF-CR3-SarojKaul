1cd4de5a981e80546b85c186d8bd99b9f4266b89
saroj kaul <70893277+sarojkaul@users.noreply.github.com>
2020-11-20 14:20:26 +0100

    Update Bike.java

--- src/citybike/Bike.java
+++ src/citybike/Bike.java
@@ -15,9 +15,8 @@ public class Bike {
 
     public static int getBikeID() {
           return bikeID;
-          //return bikeID++; tried to create here increment bikeid with creating new object but not come desire result
-    }//bikeID will increase with creating the new bike objects
-
+          
+    }
     public static String getColor() {
         return color;
     }
